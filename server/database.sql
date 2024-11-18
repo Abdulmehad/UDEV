9d9fcff3d92cb567b94ce90c73baffee974c4302
Abdul Mehad <146379929+Abdulmehad@users.noreply.github.com>
2024-11-18 23:17:46 +0100

    database connected

---
+++ server/database.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE Employees;
+-- CREATE TABLE emplyeeDetails(
+--     employee_id SERIAL PRIMARY KEY, 
+--     name VARCHAR(50),
+--     email VARCHAR(50),
+--     phone VARCHAR(50),
+--     cnic VARCHAR(50),
+-- )
+
