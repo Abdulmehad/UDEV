3477fd60e743a41678eee22760b7e89ba2e0ecd2
usama <usamaaslam888@gmail.com>
2024-11-29 19:07:40 +0500

    changes

--- server/database.sql
+++
@@ -1,9 +0,0 @@
--- CREATE DATABASE Employees;
--- CREATE TABLE emplyeeDetails(
---     employee_id SERIAL PRIMARY KEY, 
---     name VARCHAR(50),
---     email VARCHAR(50),
---     phone VARCHAR(50),
---     cnic VARCHAR(50),
--- )
-
